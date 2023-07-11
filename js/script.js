169c902828f581c796feaeada59ead6593503af4
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-07-11 17:44:38 +0300

    added changes

--- js/script.js
+++ js/script.js
@@ -58,6 +58,15 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
             'seconds': seconds
         };
     }
+
+    function getZero(num) {
+        if (num >= 0 && num <10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
     function setClock (selector, endtime) {
         const timer = document.querySelector(selector),
             days = timer.querySelector('#days'),
@@ -66,13 +75,16 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
+            updateClock();
+
+
         function updateClock() {
             const t = getTimeRemaining(endtime);
 
-            days.innerHTML = t.days;
-            hours.innerHTML = t.hours;
-            minutes.innerHTML = t.minutes;
-            seconds.innerHTML = t.seconds;
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
@@ -83,46 +95,5 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
 
     setClock('.timer', deadLine);
 
-
-    // function getTimeRemaining(endtime) {
-    //     const t = Date.parse(endtime) - Date.parse(new Date()),
-    //           days = Math.floor(t / (1000 * 60 * 60 * 24)),
-    //           hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-    //           minutes = Math.floor((t / 1000 / 60) % 60),
-    //           seconds = Math.floor((t / 1000) % 60);
-
-    //           return {
-    //             'total': t,
-    //             'days': days,
-    //             'hours': hours,
-    //             'minutes': minutes,
-    //             'seconds': seconds
-    //           };
-    //     }
-
-    //     function setClock (selector, endtime) {
-    //         const timer = document.querySelector(selector),
-    //               days = timer.querySelector('#days'),
-    //               hours = timer.querySelector('#hours'),
-    //               minutes = timer.querySelector('#minutes'),
-    //               seconds = timer.querySelector('#seconds'),
-    //               timeInterval = setInterval(updateClock, 1000);
-
-
-    //         function updateClock() {
-    //             const t = getTimeRemaining(endtime);
-
-    //             days.innerHTML = t.days;
-    //             hours.innerHTML = t.hours;
-    //             minutes.innerHTML = t.minutes;
-    //             seconds.innerHTML = t.seconds;
-
-    //             if (t.total <= 0) {
-    //                 clearInterval(timeInterval);
-    //             }
-    //         }
-    //     }
-
-    // setClock('.timer', deadLine);
 
 });
