bbacfb201c337ad26ad1fb91f0113741b5d57a73
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-07-20 15:55:14 +0300

    changed old xmlhttp to fetch + changed formData conditions to promise

--- js/script.js
+++ js/script.js
@@ -246,10 +246,12 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            // const request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        
+
+            // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
             const object = {};
@@ -257,19 +259,24 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                        statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            })
+            .then(data => data.text())
+            .then(data => {
+                 console.log(data);
+                showThanksModal(message.success);
+                // form.reset();
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             })
         });
     }
@@ -297,4 +304,5 @@ document.addEventListener('DOMContentLoaded', () => { // tabs
                 closeModal();
             }, 4000);
     }
+
 });
